c1285ec845c330780a45a97f654016b59169746b
JamesDimitrako <dimdimitrako@gmail.com>
2020-04-08 23:34:40 +0300

    Project Infrastructure

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,12 +1,77 @@
-import React, { Component } from 'react';
-import { Button } from 'antd';
+import React, { Component } from "react";
+import axios from "axios";
+import {ITicket} from "../models/ticket";
+import { Layout, Menu } from 'antd';
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
 
-class App extends Component {
+const { Header, Content, Footer, Sider } = Layout;
+
+interface IState {
+    tickets: ITicket[];
+    collapsed: boolean
+}
+
+class App extends Component<{}, IState> {
+  readonly state: IState = {
+    tickets: [],
+    collapsed: false
+  };
+
+  componentDidMount() {
+    axios.get<ITicket[]>("http://localhost:5000/api/tickets").then(response => {
+      this.setState({
+        tickets: response.data
+      });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
+<Layout>
+    <Sider
+      breakpoint="lg"
+      collapsedWidth="0"
+      onBreakpoint={broken => {
+        console.log(broken);
+      }}
+      onCollapse={(collapsed, type) => {
+        console.log(collapsed, type);
+      }}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1">
+          <UserOutlined />
+          <span className="nav-text">nav 1</span>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <VideoCameraOutlined />
+          <span className="nav-text">nav 2</span>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <UploadOutlined />
+          <span className="nav-text">nav 3</span>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <UserOutlined />
+          <span className="nav-text">nav 4</span>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout>
+      <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0' }}>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+          content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>
     );
   }
 }
